8b97bcf9ac74693dfc970c5655e8babe9756e305
Yukta Noela <yukta.noela@outlook.com>
2023-07-01 10:17:43 +0530

    complete pdf

--- App5-invoice-generator/main.py
+++ App5-invoice-generator/main.py
@@ -6,11 +6,57 @@ from pathlib import Path
 filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name='Sheet 1')
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_no = filename.split('-')[0]
+    invoice_no, date = filename.split('-')
+
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"invoice no.{invoice_no}")
+    pdf.cell(w=50, h=8, txt=f"Invoice no. : {invoice_no}", ln=1)
+
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f"Date : {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    # add a header
+    columns = list(df.columns)
+    columns = [items.replace('_', ' ').title() for items in columns]
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=str(columns[0]), border=1)
+    pdf.cell(w=70, h=8, txt=str(columns[1]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[2]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[3]), border=1)
+    pdf.cell(w=30, h=8, txt=str(columns[4]), border=1, ln=1)
+
+    # add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family='Times', size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    total = df['total_price'].sum()
+    pdf.set_font(family='Times', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=70, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt=str(total), border=1, ln=1)
+
+    # add total sum sentence
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.cell(w=30, h=8, txt=f'The total price is {total}', ln=1)
+
+    # add company name and logo
+    pdf.set_font(family='Times', size=14, style='B')
+    pdf.cell(w=25, h=8, txt=f'PythonHow')
+    pdf.image('pythonhow.png', w=10)
+
     pdf.output(f"PDFs/{filename}.pdf")
