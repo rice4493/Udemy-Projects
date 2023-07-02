2a940c081b61216f5554e4a644a44f9e19800d85
Yukta Noela <yukta.noela@outlook.com>
2023-07-02 11:34:40 +0530

    learning api's

---
+++ App6-news-api-email/main.py
@@ -0,0 +1,18 @@
+import requests
+
+# requests is useful when you want to browse an api
+
+api_key = '11ec0676293443bbabc7c85e6eb6471c'
+url = 'https://newsapi.org/v2/everything?q=tesla&' \
+      'from=2023-06-02&sortBy=publishedAt&apiKey=' \
+      '11ec0676293443bbabc7c85e6eb6471c'
+
+request = requests.get(url)
+
+# get a dictionary with data
+content = request.json()
+
+# access the article titles and description
+for article in content['articles']:
+      print(article['title'])
+      print(article['description'])
