39d531fbffca21377787f36aaffeac5acf111748
Yukta Noela <yukta.noela@outlook.com>
2023-08-09 08:02:58 +0530

    Using Jupyter

--- App7-weather-api/main.py
+++ App7-weather-api/main.py
@@ -10,8 +10,10 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    df = pandas.read_csv("")
-    return render_template("about.html")
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
 if __name__ == "__main__":
